faada7fee324a7121587890f9f1cc03b2f66541f
Brady Hammond <brady_hammond1.baylor.edu>
2022-02-11 18:59:21 -0600

    finished world series file

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,12 +1,12 @@
-words = {}
+words_dict = {}
 infile = open("AI.txt", "r")
 word_file = infile.read()
 
 for word in word_file.split():
-    if word not in words:
-        words[word] = 1
-    elif word in words:
-        words[word] += 1
-print(words)
+    if word not in words_dict:
+        words_dict[word] = 1
+    elif word in words_dict:
+        words_dict[word] += 1
+print(words_dict)
 
 infile.close()
--- WorldSeriesWinners.py
+++ WorldSeriesWinners.py
@@ -1,7 +1,29 @@
 team_wins = {}
+year_wins = {}
 infile = open("WorldSeriesWinners.txt", "r")
 win_file = infile.read()
+year = 1903
+for team in win_file.splitlines():
+    if team not in team_wins:
+        team_wins[team] = 1
+    elif team in team_wins:
+        team_wins[team] += 1
+    year_wins[year] = team
+    year += 1
+    if year in [1904, 1994]:
+        year += 1
 
-for line in win_file:
-    print(line, end="")
-print(team_wins)
+result = int(input("Choose a year in between 1903 and 2008: "))
+while result == 1904 or result == 1994:
+    print("The World Series was not played in", result)
+    result = int(input("Choose a year in between 1903 and 2008: "))
+print(
+    "The winner of the",
+    result,
+    "World Series was the",
+    year_wins[result],
+    "who have won the World Series",
+    team_wins[year_wins[result]],
+    "times.",
+)
+infile.close()
